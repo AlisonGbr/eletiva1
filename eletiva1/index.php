1d43ecc2c28110de4743bc3ef8403385dd426a5e
AlisonGbr <alisongbr.ofc@gmail.com>
2026-08-13 21:35:40 -0300

    Add files via upload

---
+++ eletiva1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividades de Eletiva</title>
+</head>
+<body>
+    <p>Ano: 2025</p>
+    <p>Ano: <?php echo date("Y"); ?></p>
+    <p><a href="exercicio1/exercicio.php">Exercicio 1</a></p>
+    <p><a href="exercicio2/exercicio2.php">Exercicio 2</a></p>
+    <p><a href="exercicio3/exercicio3.php">Exercicio 3</a></p>
+    <p><a href="exercicio4/exercicio4.php">Exercicio 4</a></p>
+</body>
+</html>
