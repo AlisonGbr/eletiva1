3153efda88e78c033567b19622d290c9640e29e0
AlisonGbr <alisongbr.ofc@gmail.com>
2026-09-06 16:07:07 -0300

    Implementação do exercício 6 e inserção do link no index.html

---
+++ eletiva1/exercicio6/exercicio6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 6</title>
+</head>
+<body>
+    <h1>Conversão Fahrenheit para Celsius</h1>
+    <form method="post" action="resposta6.php">
+        <label for="fahrenheit">Informe a temperatura em Fahrenheit: </label>
+        <input type="number" id="fahrenheit" name="fahrenheit">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
--- eletiva1/index.php
+++ eletiva1/index.php
@@ -13,5 +13,6 @@
     <p><a href="exercicio3/exercicio3.php">Exercicio 3</a></p>
     <p><a href="exercicio4/exercicio4.php">Exercicio 4</a></p>
     <p><a href="exercicio5/exercicio5.php">Exercicio 5</a></p>
+    <p><a href="exercicio6/exercicio6.php">Exercicio 6</a></p>
 </body>
 </html>
