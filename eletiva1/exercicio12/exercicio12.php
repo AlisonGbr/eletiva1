e8471eb489378444def7ef3da3d47b50e1f4bdb8
Alisongbr <alisongbr.ofc@gmail.com>
2026-09-10 19:54:43 -0300

    Implementação do exercício 12 e inserção do link no index.php

---
+++ eletiva1/exercicio12/exercicio12.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 12</title>
+</head>
+<body>
+    <h1>Conversão Metros em Centímetros</h1>
+    <form method="post" action="resposta12.php">
+        <label for="metros">Informe a quantidade em metros: </label>
+        <input type="number" id="metros" name="metros" step="0.01">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
