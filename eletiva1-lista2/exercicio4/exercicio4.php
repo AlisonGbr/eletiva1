77d67843e82c8ced7a47cb8d6c403d8e51ad568b
AlisonGbr <alisongbr.ofc@gmail.com>
2026-09-16 21:56:01 -0300

    Implementação do exercício 4, lista 2 e inserção do link no index.php

---
+++ eletiva1-lista2/exercicio4/exercicio4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 4</title>
+</head>
+<body>
+    <h2>Verificação de Mês com base no número</h2>
+    <form method="post" action="resposta4.php">
+        <label for="mes">Digite um número de 1 a 12:</label>
+        <input type="number" id="mes" name="mes" min="1" max="12">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
