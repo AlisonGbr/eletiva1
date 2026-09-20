1504fe92fb94fd8da31a25d0159ac67ddc88cc9e
AlisonGbr <alisongbr.ofc@gmail.com>
2026-09-20 14:45:40 -0300

    Implementação do exercício 5, lista 2 e inserção do link no index.php

---
+++ eletiva1-lista2/exercicio5/exercicio5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5</title>
+</head>
+<body>
+    <h2>Loop até um número</h2>
+    <form method="post" action="resposta5.php">
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
