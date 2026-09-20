3800a6fd0837bba78c671cfa0914748bc8de6be4
AlisonGbr <alisongbr.ofc@gmail.com>
2026-09-20 15:06:03 -0300

    Implementação do exercício 9, lista 2 e inserção do link no index.php

---
+++ eletiva1-lista2/Exercicio9/exercicio9.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 9</title>
+</head>
+<body>
+    <h2>Tabuada</h2>
+    <form action="resposta9.php" method="post">
+        <label for="numero">Digite um número:</label>
+        <input type="number" name="numero" id="numero" required>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ eletiva1-lista2/Exercicio9/resposta9.php
@@ -0,0 +1,8 @@
+<?php
+    $numero = $_POST['numero'];
+    echo "<h2>Tabuada do $numero</h2>";
+    for ($i = 1; $i <= 10; $i++) {
+        $resultado = $numero * $i;
+        echo "<p>$numero x $i = $resultado</p>";
+    }
+?>
